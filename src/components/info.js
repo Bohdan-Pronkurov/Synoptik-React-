e1e631e5add6187ba978e27ffecce8ccdb3fd4ef
Breez17 <bogdanpronkurov@gmail.com>
2019-11-03 01:32:57 +0200

    first commit react synoptik

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Погодное положение</h2>
+                <p> Узнать погоду в вашем городе</p>
+            </div>
+        )
+    }
+}
+
+export default Info;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import './index';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+    document.getElementById('root')
+);
