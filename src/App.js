0cf9ccf4bdc46235a36d40a9217b86de4071934a
Breez17 <bogdanpronkurov@gmail.com>
2019-11-03 02:04:01 +0200

    fix some bug with sunset, rmv sunrise add pressure

--- src/App.js
+++ src/App.js
@@ -3,51 +3,58 @@ import Info from "./components/info";
 import Form from "./components/form";
 import Weather from "./components/weather";
 
-const  API_KEY ='4f894b3a3aa0119dac91cbeaf6f27384';
+const API_KEY = '4f894b3a3aa0119dac91cbeaf6f27384';
 
 class App extends React.Component {
 
     state = {
-      temp: undefined,
-      city: undefined,
-      country: undefined,
-      sunrise: undefined,
-      sunset: undefined,
-      error: undefined,
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: undefined,
 
     };
 
     gettingWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
-        const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-        const data = await api_url.json();
 
-        if() {
 
+        if (city) {
+            const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await api_url.json();
+
+            let sunset = data.sys.sunset;
+            let date = new Date();
+            date.setTime(sunset);
+            let sunset_date = date.getHours() + ":"+ date.getMinutes() + ":" + date.getSeconds();
+
+
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                pressure: data.main.pressure,
+                sunset: sunset_date,
+                error: "",
+            });
         }
-        this.setState({
-            temp: data.main.temp,
-            city: data.name,
-            country: data.sys.country,
-            sunrise: data.sys.sunrise,
-            sunset: data.sys.sunset,
-            error: "",
-        });
     };
 
     render() {
         return (
             <div>
-                <Info />
-                <Form weatherMethod={this.gettingWeather} />
+                <Info/>
+                <Form weatherMethod={this.gettingWeather}/>
                 <Weather
-                temp={this.state.temp}
-                city={this.state.city}
-                country={this.state.country}
-                sunrise={this.state.sunrise}
-                sunset={this.state.sunset}
-                error={this.state.error}
+                    temp={this.state.temp}
+                    city={this.state.city}
+                    country={this.state.country}
+                    pressure={this.state.pressure}
+                    sunset={this.state.sunset}
+                    error={this.state.error}
                 />
             </div>
         );
